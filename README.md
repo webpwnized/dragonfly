7484bb2f4a6faa9bf011df761610bddd04c64aa4
webpwnized <webpwnized@gmail.com>
2023-12-30 19:42:49 -0500

    0.0.3 Update application code

--- README.md
+++ README.md
@@ -7,4 +7,18 @@
                  /____/                  /____/
 ```
 
-A web-based application designed to collect metrics to help measure browser privacy impact
+### A web-based application designed to collect metrics to help measure browser privacy impact
+
+## How to run the application in Docker
+
+ - Build the containers with [the Dragonfly Docker project](https://github.com/webpwnized/dragonfly-docker)
+  - Run with Docker Compose
+
+## How to run the application from Dockerhub
+
+ - The Docker Compose file is available in [the Dragonfly Dockerhub project](https://github.com/webpwnized/dragonfly-dockerhub)
+ - Run the pre-built containers from DockerHub using Docker Compose 
+
+ ## How to deploy the application to Google Cloud
+
+  - The Terraform Infrastructure as Code (IaC) is available from [Dragonfly Terraform project](https://github.com/webpwnized/dragonfly-terraform)
--- index.php
+++ index.php
@@ -1,24 +1,52 @@
 <?php
-	// create a function to get the client IP
-  function get_client_ip_address() {
-      $l_client_ip_address = '';
-      if (getenv('HTTP_CLIENT_IP'))
-          $l_client_ip_address = getenv('HTTP_CLIENT_IP');
-      else if(getenv('HTTP_X_FORWARDED_FOR'))
-          $l_client_ip_address = getenv('HTTP_X_FORWARDED_FOR');
-      else if(getenv('HTTP_X_FORWARDED'))
-          $l_client_ip_address = getenv('HTTP_X_FORWARDED');
-      else if(getenv('HTTP_FORWARDED_FOR'))
-          $l_client_ip_address = getenv('HTTP_FORWARDED_FOR');
-      else if(getenv('HTTP_FORWARDED'))
-         $l_client_ip_address = getenv('HTTP_FORWARDED');
-      else if(getenv('REMOTE_ADDR'))
-          $l_client_ip_address = getenv('REMOTE_ADDR');
-      else
-          $l_client_ip_address = 'UNKNOWN';
-      return $l_client_ip_address;
-  }
+    // create a function to get the client IP
+    function get_client_ip_address() {
+        $l_client_ip_address = "";
+
+        if (getenv('HTTP_CLIENT_IP'))
+            $l_client_ip_address = getenv('HTTP_CLIENT_IP');
+        else if(getenv('HTTP_X_FORWARDED_FOR'))
+            $l_client_ip_address = getenv('HTTP_X_FORWARDED_FOR');
+        else if(getenv('HTTP_X_FORWARDED'))
+            $l_client_ip_address = getenv('HTTP_X_FORWARDED');
+        else if(getenv('HTTP_FORWARDED_FOR'))
+            $l_client_ip_address = getenv('HTTP_FORWARDED_FOR');
+        else if(getenv('HTTP_FORWARDED'))
+            $l_client_ip_address = getenv('HTTP_FORWARDED');
+        else if(getenv('REMOTE_ADDR'))
+            $l_client_ip_address = getenv('REMOTE_ADDR');
+        else
+            $l_client_ip_address = 'UNKNOWN';
+        return $l_client_ip_address;
+    }
   
-  // print ip address
-  echo get_client_ip_address();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Dragonfly</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+
+    <body>
+        <h1>Dragonfly</h1>
+        <table class="styled-table">
+            <thead>
+                <tr>
+                    <th>Data Point</th>
+                    <th>Data</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Client IP Address</td>
+                    <td><?php echo get_client_ip_address(); ?></td>
+                </tr>
+            </tbody>
+        </table>
+    </body>
+</html>
