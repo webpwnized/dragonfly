cc7bb1947d0b244a2dc33ae65d469b1b7e0cf303
webpwnized <webpwnized@gmail.com>
2023-12-30 20:14:37 -0500

    0.0.3 Update application code

--- index.php
+++ index.php
@@ -1,26 +1,41 @@
 <?php
-    // create a function to get the client IP
-    function get_client_ip_address() {
-        $l_client_ip_address = "";
 
-        if (getenv('HTTP_CLIENT_IP'))
-            $l_client_ip_address = getenv('HTTP_CLIENT_IP');
-        else if(getenv('HTTP_X_FORWARDED_FOR'))
-            $l_client_ip_address = getenv('HTTP_X_FORWARDED_FOR');
-        else if(getenv('HTTP_X_FORWARDED'))
-            $l_client_ip_address = getenv('HTTP_X_FORWARDED');
-        else if(getenv('HTTP_FORWARDED_FOR'))
-            $l_client_ip_address = getenv('HTTP_FORWARDED_FOR');
-        else if(getenv('HTTP_FORWARDED'))
-            $l_client_ip_address = getenv('HTTP_FORWARDED');
-        else if(getenv('REMOTE_ADDR'))
-            $l_client_ip_address = getenv('REMOTE_ADDR');
-        else
-            $l_client_ip_address = 'UNKNOWN';
-        return $l_client_ip_address;
-    }
-  
+    $l_http_client_ip_address = "";
+    $l_http_x_forwarded_for = "";
+    $l_http_x_forwarded = "";
+    $l_http_forwarded_for = "";
+    $l_http_forwarded = "";
+    $l_http_remote_address = "";
+    
+
+    if (getenv('HTTP_CLIENT_IP')){
+        $l_client_ip_address = getenv('HTTP_CLIENT_IP');
+    };
+        
+    if(getenv('HTTP_X_FORWARDED_FOR')){
+        $l_http_x_forwarded_for = getenv('HTTP_X_FORWARDED_FOR');
+    };
+
+    if(getenv('HTTP_X_FORWARDED')){
+        $l_http_x_forwarded = getenv('HTTP_X_FORWARDED');
+    };
+    
+    if(getenv('HTTP_FORWARDED_FOR')){
+        $l_http_forwarded_for = getenv('HTTP_FORWARDED_FOR');
+    };
+
+    if(getenv('HTTP_FORWARDED')){
+        $l_http_forwarded = getenv('HTTP_FORWARDED');
+    };  
+
+    if(getenv('HTTP_REMOTE_ADDR')){
+        $l_http_remote_address = getenv('HTTP_REMOTE_ADDR');
+    };  
+
+    phpinfo();
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -32,7 +47,7 @@
         <link rel="stylesheet" href="styles.css">
     </head>
 
-    <body>
+    <body class="content">
         <h1>Dragonfly</h1>
         <table class="styled-table">
             <thead>
@@ -43,8 +58,28 @@
             </thead>
             <tbody>
                 <tr>
-                    <td>Client IP Address</td>
-                    <td><?php echo get_client_ip_address(); ?></td>
+                    <td>HTTP Client IP Address</td>
+                    <td><?php echo $l_http_client_ip_address; ?></td>
+                </tr>
+                <tr>
+                    <td>X Forwarded For</td>
+                    <td><?php echo $l_http_x_forwarded_for; ?></td>
+                </tr>
+                <tr>
+                    <td>X Forwarded</td>
+                    <td><?php echo $l_http_x_forwarded; ?></td>
+                </tr>
+                <tr>
+                    <td>Forwarded For</td>
+                    <td><?php echo $l_http_forwarded_for; ?></td>
+                </tr>
+                <tr>
+                    <td>Forwarded</td>
+                    <td><?php echo $l_http_forwarded; ?></td>
+                </tr>
+                <tr>
+                    <td>HTTP Remote IP Address</td>
+                    <td><?php echo $l_http_remote_address; ?></td>
                 </tr>
             </tbody>
         </table>
