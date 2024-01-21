820a661efe4b8ef06e16deb424191dc6bdd08b07
webpwnized <webpwnized@gmail.com>
2024-01-21 17:43:22 -0500

    0.0.5 Update application

--- index.php
+++ index.php
@@ -106,10 +106,10 @@
         </script>
     </head>
     <body class="content">
-        <p`>
+        <div class="page-header">
             <img src="images/blue-dragonfly-icon.jpg" height="75px" width="75px" />
-            Dragonfly
-        </p>
+            <span>Dragonfly</span>
+        </div>
         <table class="styled-table">
             <thead>
                 <tr>
